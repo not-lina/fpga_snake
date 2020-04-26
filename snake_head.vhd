c8b5e018205e83fbfe972b01c23c2bd28f0253cf
lina-lp <lina@lilypad.co>
2020-04-25 21:46:14 -0400

    fixed whitespace

--- snake_head.vhd
+++ snake_head.vhd
@@ -35,32 +35,34 @@ architecture Behavioral of snake_head is
 	signal move, move_next: std_logic := '0';
 
 begin
-  move_snake: process(tick, mv_state) begin
-		case(mv_state) is
-				when m_hold =>
-					if(tick = '1')
-					then mv_state_next <= m_ready;
-					else mv_state_next <= m_hold;
-					end if;
-					move_next <= '0';
+   -- handshake for animations
+    move_snake: process(tick, mv_state) begin
+        case(mv_state) is
+            when m_hold =>
+                if(tick = '1')
+                then mv_state_next <= m_ready;
+                else mv_state_next <= m_hold;
+                end if;
+                move_next <= '0';
 
-				when m_ready =>
-					mv_state_next <= m_trigger;
-					move_next <= '1';
+            when m_ready =>
+                mv_state_next <= m_trigger;
+                move_next <= '1';
 
-				when m_trigger =>
-					if(tick = '0')
-					then mv_state_next <= m_hold;
-					else mv_state_next <= m_trigger;
-					end if;
-					move_next <= '0';
+            when m_trigger =>
+                if(tick = '0')
+                then mv_state_next <= m_hold;
+                else mv_state_next <= m_trigger;
+                end if;
+                move_next <= '0';
 
-				when others =>
-					mv_state_next <= m_hold;
-					move_next <= '0';
-			end case;
-  end process;
+            when others =>
+                mv_state_next <= m_hold;
+                move_next <= '0';
+        end case;
+    end process;
 
+    -- state register and fsm tick
 	reg_state: process(clock) begin
 		 if rising_edge(clock) then
 			  state <= nextstate;
@@ -73,60 +75,63 @@ begin
 		 end if;
 	end process;
 
-	OUTPUT_DECODE:
-    process (state, head_pos_x, head_pos_y, path) begin
+	output_decode:
+	process (state, head_pos_x, head_pos_y, path) begin
         case (state) is
             when init =>
                 head_pos_x_next <= 22;
-					 head_pos_y_next <= 22;
-					 moved_next <= '0';
-					 path_next <= up_d;
+                head_pos_y_next <= 22;
+                moved_next <= '0';
+                path_next <= up_d;
 
             when up_go =>
-					 head_pos_x_next <= head_pos_x;
+                head_pos_x_next <= head_pos_x;
                 head_pos_y_next <= head_pos_y + 1;
-					 moved_next <= '1';
-					 path_next <= up_d;
+                moved_next <= '1';
+                path_next <= up_d;
 
             when down_go =>
-					 head_pos_x_next <= head_pos_x;
+                head_pos_x_next <= head_pos_x;
                 head_pos_y_next <= head_pos_y - 1;
-					 moved_next <= '1';
-					 path_next <= down_d;
+                moved_next <= '1';
+                path_next <= down_d;
 
-				when right_go =>
-					head_pos_x_next <= head_pos_x + 1;
-               head_pos_y_next <= head_pos_y;
-					moved_next <= '1';
-					path_next <= right_d;
+            when right_go =>
+                head_pos_x_next <= head_pos_x + 1;
+                head_pos_y_next <= head_pos_y;
+                moved_next <= '1';
+                path_next <= right_d;
 
-				when left_go =>
-					head_pos_x_next <= head_pos_x - 1;
-               head_pos_y_next <= head_pos_y;
-					moved_next <= '1';
-					path_next <= left_d;
+            when left_go =>
+                head_pos_x_next <= head_pos_x - 1;
+                head_pos_y_next <= head_pos_y;
+                moved_next <= '1';
+                path_next <= left_d;
 
             when others =>
                 head_pos_x_next <= head_pos_x;
-					 head_pos_y_next <= head_pos_y;
-					 moved_next <= '0';
-					 path_next <= path;
-            end case;
+                head_pos_y_next <= head_pos_y;
+                moved_next <= '0';
+                path_next <= path;
+        end case;
     end process;
 
-    NEXT_STATE_DECODE:
+    next_state_decode:
     process(state, move, path, UP, DOWN, LEFT, RIGHT, RESET, START) begin
+        -- async reset
 		if(RESET = '0') then nextstate <= init;
+
+		-- animation ticks take presidence over user button presses
 		elsif (move = '1') then
-					case (path) is
-						when up_d => nextstate <= up_go;
-						when down_d => nextstate <= down_go;
-						when left_d => nextstate <= left_go;
-						when right_d => nextstate <= right_go;
-						when others => nextstate <= state;
-					end case;
-		else
-        case (state) is
+		    case (path) is
+                when up_d => nextstate <= up_go;
+                when down_d => nextstate <= down_go;
+                when left_d => nextstate <= left_go;
+                when right_d => nextstate <= right_go;
+                when others => nextstate <= state;
+            end case;
+
+		else case (state) is
             when init => nextstate <= iwait;
 
             when iwait =>
@@ -139,45 +144,39 @@ begin
 
             when up_go => nextstate <= up_wait;
 
-				when up_wait =>
-					if(UP='0') then
-						nextstate <= up_wait;
-					else
-						nextstate <= iwait;
-					end if;
+            when up_wait =>
+                if(UP='0')
+                then nextstate <= up_wait;
+                else nextstate <= iwait;
+                end if;
 
             when down_go => nextstate <= down_wait;
 
-				when down_wait =>
-					if(DOWN='0') then
-						nextstate <= down_wait;
-					else
-						nextstate <= iwait;
-					end if;
+            when down_wait =>
+                if(DOWN='0')
+                then nextstate <= down_wait;
+                else nextstate <= iwait;
+                end if;
 
-				when left_go => nextstate <= left_wait;
+            when left_go => nextstate <= left_wait;
 
-				when left_wait =>
-					if(LEFT='0') then
-						nextstate <= left_wait;
-					else
-						nextstate <= iwait;
-					end if;
+            when left_wait =>
+                if(LEFT='0')
+                then nextstate <= left_wait;
+                else nextstate <= iwait;
+                end if;
 
 				when right_go => nextstate <= right_wait;
 
-				when right_wait =>
-					if(RIGHT='0') then
-						nextstate <= right_wait;
-					else
-						nextstate <= iwait;
-					end if;
+            when right_wait =>
+                if(RIGHT='0')
+                then nextstate <= right_wait;
+                else nextstate <= iwait;
+                end if;
 
-            when others =>
-                nextstate <= init;
-
+            when others => nextstate <= init;
         end case;
-	  end if;
+	    end if;
     end process;
 
 	pos_x <= head_pos_x;
